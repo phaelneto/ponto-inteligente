ea18751066ae8fcf38d788492978b64251bbf3e7
Raphael Neto <phaelneto@hotmail.com>
2020-07-17 15:34:47 -0300

    Ajuste Profile

--- src/test/java/br/com/home/pontointeligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/br/com/home/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -2,8 +2,12 @@ package br.com.home.pontointeligente.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.annotation.ProfileValueSourceConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PontoInteligenteApplicationTests {
 
 	@Test
